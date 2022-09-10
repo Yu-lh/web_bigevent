745be66494c247eec9c8795e182fd511208ea3ca
chenwj <1765189937@qq.com>
2022-09-10 22:30:35 +0800

    完成个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,50 @@
+$(function () {
+    var form = layui.form
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在1~6个字符之间'
+            }
+        }
+    })
+
+    initUserInfo()
+
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                console.log(res);
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    // 表单重置功能
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        console.log('重置')
+        initUserInfo()
+    })
+
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+                // 调用父页面的方法，重新渲染信息
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
